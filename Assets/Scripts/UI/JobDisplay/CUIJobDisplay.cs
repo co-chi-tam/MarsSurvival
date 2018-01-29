07c27936ba5b4449219ab357e8944b21e412cf80
Co Chi Tam <cochitam1990@gmail.com>
2018-01-29 15:56:19 +0700

    + Update code + New build.

--- Assets/Scripts/UI/JobDisplay/CUIJobDisplay.cs
+++ Assets/Scripts/UI/JobDisplay/CUIJobDisplay.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CUIJobDisplay : MonoBehaviour {
 
 	[Header("Configs")]
+	[SerializeField]	protected float m_DisplayRadius = 20f;
 	[SerializeField]	protected CUIJobDisplayItem[] m_JobItems;
 
 	public virtual void SetUpJobDisplay(string[] jobItems) {
@@ -14,15 +15,21 @@ public class CUIJobDisplay : MonoBehaviour {
 			var jobItem = this.m_JobItems [x];
 			jobItem.jobGameObject.SetActive (false);
 		}
+		var segment = (Mathf.PI * 2f) / jobItems.Length;
+		var theta = 0f;
 		for (int i = 0; i < jobItems.Length; i++) {
 			var jobName = jobItems [i];
-			for (int x = 0; x < this.m_JobItems.Length; x++) {
-				var jobItem = this.m_JobItems [x];
+			var x = Mathf.Sin (theta) * this.m_DisplayRadius;
+			var y = Mathf.Cos (theta) * this.m_DisplayRadius;
+			for (int a = 0; a < this.m_JobItems.Length; a++) {
+				var jobItem = this.m_JobItems [a];
 				if (jobName == jobItem.jobName) {
 					jobItem.jobGameObject.SetActive (true);
+					jobItem.SetUp (new Vector2 (x, y));
 					break;
 				} 
 			}
+			theta += segment;
 		}
 	}
 
--- Assets/Scripts/UI/JobDisplay/CUIJobDisplayItem.cs
+++ Assets/Scripts/UI/JobDisplay/CUIJobDisplayItem.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 [Serializable]
 public class CUIJobDisplayItem {
 
+	#region Main methods
+
 	[Header("Configs")]
 	public string jobName;
 	public GameObject jobGameObject;
+
+	#endregion
+
+	#region Main methods
+
+	public virtual void SetUp(Vector2 pos) {
+		var rectTransform = this.jobGameObject.transform as RectTransform;
+		rectTransform.anchoredPosition = pos;
+	}
+
+	#endregion
 
 }
